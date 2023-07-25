c123d092c659e652dac8e5f5dad0f3d3224575e1
Ujjwal dhungel <dhungelujjwal441@gmail.com>
2023-07-25 19:21:15 +0545

    comment added

--- lib/feature/news/presentation/all_news_page.dart
+++ lib/feature/news/presentation/all_news_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/feature/news/presentation/news_page.dart';
-
+//git remote set-url origin https://github.com/dhungel123/newsApp.git
+//$ ssh-keygen -t rsa -b 4096 -C "dhungelujjwal441@gmail.com"
 class AllNewsPage extends StatelessWidget {
   const AllNewsPage({Key? key}) : super(key: key);
 
